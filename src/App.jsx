1a361d060a8b7d82e1050d8d9b317513b579a053
Jvbass <juan.vpinoc@gmail.com>
2022-12-14 00:32:41 -0300

    |fixed|json price  |added|checkout view, carrito styles, carrito total

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,96 @@
-import './App.css'
+import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-import ProtectedRoute from './components/ProtectedRoute.jsx'
-import NavBar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx';
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import NavBar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 //views
-import Carrito from './views/Carrito.jsx'
-import Bordados from './views/Bordados.jsx'
-import CasaPajaros from './views/Casapajaros.jsx'
-import Home from './views/Home.jsx'
-import Lamparas from './views/Lamparas.jsx'
-import NotFound from './views/Notfound.jsx'
-import QuienesSomos from './views/QuienesSomos.jsx'
-import Contactanos from './views/Contactanos.jsx'
-import Detail from './views/detail.jsx';
-import Buscar from './views/Buscar.jsx'
-import Login from './views/Login';
-import Admin from './views/Admin';
+import Carrito from "./views/Carrito.jsx";
+import Bordados from "./views/Bordados.jsx";
+import CasaPajaros from "./views/Casapajaros.jsx";
+import Home from "./views/Home.jsx";
+import Lamparas from "./views/Lamparas.jsx";
+import NotFound from "./views/Notfound.jsx";
+import QuienesSomos from "./views/QuienesSomos.jsx";
+import Contactanos from "./views/Contactanos.jsx";
+import Detail from "./views/detail.jsx";
+import Buscar from "./views/Buscar.jsx";
+import Login from "./views/Login";
+import Admin from "./views/Admin";
+import Checkout from "./views/Checkout.jsx";
 
-import Context from './context/context.js'
-
+import Context from "./context/context.js";
 
 function App() {
   //creamos los estados, solicitamos la data a la api, clasificamos por tipo de producto y la pasamos a un estado global.
-  const [casas, setCasas] = useState([])
-  const [lamparas, setLamparas] = useState([])
-  const [bordados, setBordados] = useState([])
-  const [todos, setTodos] = useState([])
-  const [cart, setCart] = useState([])
+  const [casas, setCasas] = useState([]);
+  const [lamparas, setLamparas] = useState([]);
+  const [bordados, setBordados] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [cart, setCart] = useState([]);
   const dataProductos = "/productos.json";
 
   useEffect(() => {
     fetch(dataProductos)
       .then((res) => res.json())
       .then((json) => {
-        setCasas(json.casasnido)
-        setLamparas(json.lamparas)
-        setBordados(json.bordados)
-        setTodos(json.todos)
+        setCasas(json.casasnido);
+        setLamparas(json.lamparas);
+        setBordados(json.bordados);
+        setTodos(json.todos);
       })
-      .catch((e) => console.log(e))
+      .catch((e) => console.log(e));
   }, []);
 
   const addToCart = (item) => {
-    const itemIndex = cart.findIndex((product) => product.id == item.id)
-    const updateCart = [...cart]
+    const itemIndex = cart.findIndex((product) => product.id == item.id);
+    const updateCart = [...cart];
     if (itemIndex === -1) {
       const product = {
         id: item.id,
         count: 1,
         price: item.price,
         img1: item.img1,
-        name: item.name
-      }
-      updateCart.push(product)
+        name: item.name,
+      };
+      updateCart.push(product);
     } else {
-      updateCart[itemIndex].count += 1
+      updateCart[itemIndex].count += 1;
     }
-    setCart(updateCart)
-  }
+    setCart(updateCart);
+  };
 
-  const globalState = { todos, casas, lamparas, bordados, cart, addToCart }
+  const removeFromCart = (item) => {
+    const itemIndex = cart.findIndex((product) => product.id === item.id);
+    const updateCart = [...cart];
+
+    updateCart[itemIndex].count -= 1;
+    if (updateCart[itemIndex].count <= 0) {
+      updateCart.splice(itemIndex, 1);
+    }
+    setCart(updateCart);
+  };
+
+  const cartTotal = () => {
+    let total = 0;
+    cart.forEach((item) => (total += item.count * item.price));
+
+    return total;
+  };
+
+  const globalState = {
+    todos,
+    casas,
+    lamparas,
+    bordados,
+    cart,
+    addToCart,
+    removeFromCart,
+    cartTotal,
+  };
 
   return (
     <div className="App">
@@ -71,29 +98,32 @@ function App() {
         <BrowserRouter>
           <NavBar></NavBar>
           <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/carrito' element={<Carrito />}></Route>
-            <Route path='/bordados' element={<Bordados />}></Route>
-            <Route path='/casapajaros' element={<CasaPajaros />} ></Route>
-            <Route path='/lamparas' element={<Lamparas />} ></Route>
-            <Route path='/quienessomos' element={<QuienesSomos />}></Route>
-            <Route path='/contactanos' element={<Contactanos />}></Route>
-            <Route path='/buscar' element={<Buscar />}></Route>
-            <Route path='*' element={<NotFound />}></Route>
-            <Route path='/:category/:name' element={<Detail />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/admin' element={
-              <ProtectedRoute>
-                <Admin></Admin>
-              </ProtectedRoute>
-            }></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/carrito" element={<Carrito />}></Route>
+            <Route path="/bordados" element={<Bordados />}></Route>
+            <Route path="/casapajaros" element={<CasaPajaros />}></Route>
+            <Route path="/lamparas" element={<Lamparas />}></Route>
+            <Route path="/quienessomos" element={<QuienesSomos />}></Route>
+            <Route path="/contactanos" element={<Contactanos />}></Route>
+            <Route path="/buscar" element={<Buscar />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+            <Route path="/:category/:name" element={<Detail />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/checkout" element={<Checkout />} />
+            <Route
+              path="/admin"
+              element={
+                <ProtectedRoute>
+                  <Admin></Admin>
+                </ProtectedRoute>
+              }
+            ></Route>
           </Routes>
           <Footer></Footer>
         </BrowserRouter>
       </Context.Provider>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/views/Buscar.jsx
+++ src/views/Buscar.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 const Busqueda = () => {
   const { todos } = useContext(Context); // data para filtrar
-  const [value, setValue] = useState(""); // data string ingresada por imput
+  const [value, setValue] = useState("/"); // data string ingresada por imput
   const [result, setResult] = useState([]); // resultado de la busqueda en array
 
   const navigate = useNavigate();
@@ -15,7 +15,11 @@ const Busqueda = () => {
     return lowerCase.replaceAll(" ", "-");
   };
   useEffect(() => {
-    filterData();
+    if (value == "") {
+      setValue("/");
+    } else {
+      filterData();
+    }
   }, [value]);
 
   const filterData = () => {
@@ -34,33 +38,46 @@ const Busqueda = () => {
 
   return (
     <>
-
-
       <div className="buscar">
-      <div className="search-box">
-                <input className="buscador"type="text" required onChange={(e) => setValue(e.target.value)}></input>
-                <i className="fa fa-search"></i>
-            </div>
-    
+        <div className="search-box">
+          <input
+            className="buscador"
+            type="text"
+            required
+            onChange={(e) => setValue(e.target.value)}
+          ></input>
+          <i className="fa fa-search"></i>
+        </div>
       </div>
 
       <h4 className="descripcion">{`${result.length} Productos encontrados`}</h4>
-        <div className="galeria grid-columns-4 p-3">
-          {result.map((filtrado) => {
-            return (
-              <div key={filtrado.id} className="card">
-                <div id="cf">
-                  <img className="bottom"src={filtrado.img1} alt={filtrado.name}></img>
-                  <img className="top" src={filtrado.img2} alt={filtrado.name} onClick={() => navigate(`/${filtrado.type}/${createRoute(filtrado.id)}`)}  ></img>
-                </div>
-                <div className="card-detail">
-                  <h4>{filtrado.name}</h4>
-                  <p>${formatPrice(filtrado.price)}</p>
-                </div>
+      <div className="galeria grid-columns-4 p-3">
+        {result.map((filtrado) => {
+          return (
+            <div key={filtrado.id} className="card">
+              <div id="cf">
+                <img
+                  className="bottom"
+                  src={filtrado.img1}
+                  alt={filtrado.name}
+                ></img>
+                <img
+                  className="top"
+                  src={filtrado.img2}
+                  alt={filtrado.name}
+                  onClick={() =>
+                    navigate(`/${filtrado.type}/${createRoute(filtrado.id)}`)
+                  }
+                ></img>
               </div>
-            );
-          })}
-        </div>
+              <div className="card-detail">
+                <h4>{filtrado.name}</h4>
+                <p>${formatPrice(filtrado.price)}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
