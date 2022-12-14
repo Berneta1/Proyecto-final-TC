08ed861837dbcefd6a2e007f6db9c0f547537b0c
Berna <bernaigna@gmail.com>
2022-12-14 18:25:53 -0300

    ajuste css notfound y boton

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -11,7 +11,7 @@ const NotFound = () => {
       />
       <span className='fs-1 fw-bold'>404!</span>
       <Link to="/">
-        <button className="btn btn-primary">Volver al home</button>
+        Volver al home
       </Link>
     </div>
   );
